c86339adb5a1a33de7724dfcba2660454f43ccc8
Sergey <we_ew_2015@mail.ru.ru>
2026-01-10 09:22:01 +0300

    исправление теста №21

--- server/src/test/java/integration/ItemRequestServiceIntegrationTest.java
+++ server/src/test/java/integration/ItemRequestServiceIntegrationTest.java
@@ -1,6 +1,5 @@
 package integration;
 
-import config.AppConfig;
 import config.PersistenceConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
