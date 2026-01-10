d19906f2ce9edaf72c2898993eac9fe74bdc0a65
Sergey <we_ew_2015@mail.ru.ru>
2026-01-10 08:03:29 +0300

    исправление теста №8

--- server/src/test/java/integration/UserServiceIntegrationTest.java
+++ server/src/test/java/integration/UserServiceIntegrationTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.transaction.annotation.Transactional;
 
