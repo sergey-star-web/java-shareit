14f9cf9a0b908f0fcf1b9d9c1a05b820039e62a0
Sergey <we_ew_2015@mail.ru.ru>
2026-01-10 07:35:18 +0300

    исправление теста №2

--- gateway/src/test/java/json/BookItemRequestDtoJsonTest.java
+++ gateway/src/test/java/json/BookItemRequestDtoJsonTest.java
@@ -37,8 +37,8 @@ public class BookItemRequestDtoJsonTest {
         // Проверки
         assertThat(content).extractingJsonPathNumberValue("$.itemId").isEqualTo(123);
         assertThat(content).extractingJsonPathStringValue("$.start")
-                .isEqualTo(startDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS")));
+                .isEqualTo(startDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSS")));
         assertThat(content).extractingJsonPathStringValue("$.end")
-                .isEqualTo(endDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS")));
+                .isEqualTo(endDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSS")));
     }
 }
