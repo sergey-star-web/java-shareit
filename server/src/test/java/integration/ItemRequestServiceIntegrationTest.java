ba1f3bc8fd843fdf14a086f3f2fcf20bac0691d3
Sergey <we_ew_2015@mail.ru.ru>
2026-01-10 10:27:12 +0300

    исправление теста №29

--- server/src/test/java/integration/ItemRequestServiceIntegrationTest.java
+++ server/src/test/java/integration/ItemRequestServiceIntegrationTest.java
@@ -4,6 +4,7 @@ import config.PersistenceConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.BookingServiceImpl;
@@ -24,6 +25,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @Transactional
 @SpringBootTest
+@ActiveProfiles("test")
 @SpringJUnitConfig({UserServiceImpl.class, BookingServiceImpl.class, ItemServiceImpl.class,
         ItemRequestServiceImpl.class, PersistenceConfig.class})
 public class ItemRequestServiceIntegrationTest {
